f58c3014414fff88a9a582931b6452479d27a433
nelson truran <nelson@truran.dev>
2024-03-08 17:44:00 -0800

    add e.g.

--- api/util/settings.py
+++ api/util/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "HexAPI"
+    app_name: str = "WebApp"
     db_connection_string: str
     logging_level: int = logging.INFO
     model_config = SettingsConfigDict(env_file=".env")
